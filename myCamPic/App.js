d5e5825ce131a35c3addd8b0b6eb800fa9b938b2
Amit <thisisag840@gmail.com>
2022-11-22 23:45:17 +0530

    myCamPic init

---
+++ myCamPic/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  
+} from 'react-native';
+
+import {RNCamera} from 'react-native-camera';
+
+const App = () => {
+  return (
+    <>
+    <Text>hii</Text>
+    </>
+  )
+}
+
+
+export default App;
